fa39f8a1407112773bd0d2240db08a0f1540a1ea
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2020-03-07 23:12:03 +0000

    Developed FileName Parsing Function

--- file_renaming_tool/main.go
+++ file_renaming_tool/main.go
@@ -1,11 +1,31 @@
 package main
 
-func main() {
-	fileName := "nginx Log"
-	newFileName, _ := renameFile(fileName){
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
 
+func main() {
+	fileName := "ngi.nx .Log\\/?"
+	stdFileName(fileName)
 }
 
-func renameFile(fileName string) (string, error){
+func stdFileName(fileName string) {
+	newFileName := strings.ToLower(fileName)
+	slOfNewFileName := strings.Split(newFileName, ".")
+	fileExt := slOfNewFileName[len(slOfNewFileName)-1]
+	newFileName = strings.Join(slOfNewFileName[0:len(slOfNewFileName)-1], ``)
 
+	re := regexp.MustCompile(`\W`)
+	bsOfFileExt := re.ReplaceAll([]byte(fileExt), []byte(``))
+	fileExt = string(bsOfFileExt)
+	if fileExt == "" || len(slOfNewFileName) == 1 {
+		fileExt = "txt"
+	}
+
+	bsOfNewFileName := re.ReplaceAll([]byte(newFileName), []byte(``))
+	newFileName = string(bsOfNewFileName)
+	newFileName = strings.Title(newFileName) + "." + fileExt
+	fmt.Println(newFileName)
 }
