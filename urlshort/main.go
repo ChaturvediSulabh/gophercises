0e9962b1689499a690820efa2fd9d7a1e86e99af
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2020-05-06 09:53:33 +0100

    Added Fallback functionality

--- urlshort/main.go
+++ urlshort/main.go
@@ -10,19 +10,26 @@ func main() {
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
-	mapHandler := mapHandler(urlMap)
+	mapHandler := mapHandler(urlMap, fallback())
 	for k := range urlMap {
 		http.HandleFunc(k, mapHandler)
 	}
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func mapHandler(urlMap map[string]string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func mapHandler(urlMap map[string]string, fallback http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		for k, v := range urlMap {
 			if r.URL.Path == k {
 				http.Redirect(w, r, v, http.StatusFound)
 			}
 		}
-	}
+		fallback.ServeHTTP(w, r)
+	})
+}
+
+func fallback() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	})
 }
